42d68b53660c709faffa3ef918506cd98ed56b95
yslinear <yslinear@gmail.com>
2019-08-08 13:15:41 +0800

    commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -23,6 +23,8 @@
 
 </html>
 
+
+{{-- echarts --}}
 <script>
     $(document).ready(function(){
         // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
@@ -32,33 +34,7 @@
             headers:{ 'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content') },
             data: {account:"{{ $account }}", timezone:Intl.DateTimeFormat().resolvedOptions().timeZone},
             success:function(result){
-                console.log(JSON.stringify(result));
-                google.charts.load("current", {packages:["corechart"]});
-                google.charts.load('current', {'packages':['line']});
-                google.charts.load('current', {'packages':['scatter']});
-                google.charts.setOnLoadCallback(drawChart);
-                function drawChart() {
-                var data = new google.visualization.DataTable();
-                data.addColumn('number', 'week');
-                data.addColumn('timeofday', 'online');
-                // dataTable.addColumn({"label":"online",type: 'timeofday', role: 'interval'});
-                data.addColumn('timeofday', 'offline');
-                for(i=0;i<result.length;i++) {
-                    data.addRows([result[i]]);
-                    console.log(JSON.stringify(result[i]));
-                    }
-                    var options={
-                    interpolateNulls: true, legend: 'none' , title: 'online time' ,interpolateNulls : true,
-                    series: {
-                            0: {color: 'green', pointSize: 4,},
-                            1: {color: 'Lightgray' , pointSize: 2}, },
-                            hAxis: {title: 'week' ,
-                                    viewWindow:{min:0,max:6},format: '#' ,
-                                    ticks: [{v:0, f:'Sun'}, {v:1, f:'Mon'}, {v:2, f:'Tue'}, {v:3, f:'Wed'},{v:4, f:'Thu'},
-                                            {v:5, f:'Fri'}, {v:6, f:'Sat'}] },
-                            vAxis: {title: '' , format:'hh:mm a'}, };
-                            var chart=new google.visualization.ScatterChart(document.getElementById('time_chart'));
-                            chart.draw(data, options); }
+
             }
         })
     });
